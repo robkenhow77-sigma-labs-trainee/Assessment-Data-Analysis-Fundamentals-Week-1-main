9700ebb1b6647d5c2baf1abdeec17be56ce8534e
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-12-13 14:32:01 +0000

    6

--- queries/challenge_6.sql
+++ queries/challenge_6.sql
@@ -1,8 +1,14 @@
--- A query that displays the average product price (average_price) for each category from each supplier when the category name contains the letter 'm' (case-insensitive).
+-- A query that displays the average product price (average_price) for each category 
+-- from each supplier when the category name contains the letter 'm' (case-insensitive).
 
 -- The columns should be supplier, category, product_price.
 
 -- product_price should be an integer.
 
 -- Results should be sorted alphabetically by supplier and category.
+
+select category_name, avg(unit_price) from products
+left join categories using (category_id)
+where category_name ilike '%m%'
+group by category_name;
 
