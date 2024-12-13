dc71efe1ccd6d8d174011ceec18764c18564e757
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-12-13 14:25:14 +0000

    5

--- queries/challenge_5.sql
+++ queries/challenge_5.sql
@@ -2,3 +2,7 @@
 
 -- The columns should be product_name and unit_price only.
 
+select product_name from products 
+left join suppliers using (supplier_id)
+where city ilike 'London' or city ilike 'Tokyo'
+limit 5;
