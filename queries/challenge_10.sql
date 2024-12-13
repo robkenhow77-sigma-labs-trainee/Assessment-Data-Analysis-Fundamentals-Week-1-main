6d22d87634027b4d5808cc4e42c00a1f77951327
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-12-13 16:34:45 +0000

    1-9

--- queries/challenge_10.sql
+++ queries/challenge_10.sql
@@ -3,4 +3,10 @@
 -- The columns should be category_name, product_name.
 
 -- Results should be sorted alphabetically by category.
+with a as (
+select category_name, product_name, units_on_order from products
+left join categories using (category_id)
+)
+select category_name, product_name, rank() over(partition by category_name) from a
+order by rank desc;
 
--- queries/challenge_9.sql
+++ queries/challenge_9.sql
@@ -2,3 +2,5 @@
 
 -- Results should be ordered in descending order of employment year, with ties broken by sorting alphabetically by last name.
 
+select concat(title_of_courtesy,' ' ,first_name,' ' ,last_name) as employee_full_name from employees
+order by extract(year from hire_date) desc, last_name desc;
