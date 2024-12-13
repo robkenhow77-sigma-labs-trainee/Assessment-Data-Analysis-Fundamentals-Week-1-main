3f8924c77975ae5c6708c9c0e24df3566608acac
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2024-12-13 14:01:48 +0000

    1 and 2

--- queries/challenge_1.sql
+++ queries/challenge_1.sql
@@ -1,2 +1,3 @@
 -- A query that displays the total count of customers under the column "total_customers".
 
+select count(*) as total_customers from customers;
--- queries/challenge_2.sql
+++ queries/challenge_2.sql
@@ -1,2 +1,5 @@
 -- A query that displays the total integer value of all products in stock as "stock_value".
 
+select round(sum(unit_price)) as stock_value from products;
+
+
